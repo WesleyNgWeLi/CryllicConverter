26760154a3747c59c8282da141eceb3a248d6e61
Wesley <wesleyngweli@github.com>
2017-09-16 16:46:34 -0500

    Completed dictionary model

--- CryllicConverter/DictionaryModel.cs
+++ CryllicConverter/DictionaryModel.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace CryllicConverter
 {
-    class DictionaryModel
+    public class DictionaryModel
     {
+        public Dictionary<char, string> cryllicMap = new Dictionary<char, string>
+        {
+            {'a', "Ӓ" },
+            {'b', "Ҕ" },
+            {'c', "Г" },
+            {'d', "Ґ" },
+            {'e', "Ђ" },
+            {'f', "Є" },
+            {'g', "Ж" },
+            {'h', "З" },
+            {'i', "И" },
+            {'j', "Ѕ" },
+            {'k', "К" },
+            {'l', "Л" },
+            {'m', "М" },
+            {'n', "Н" },
+            {'o', "Њ" },
+            {'p', "П" },
+            {'q', "С́" },
+            {'r', "У" },
+            {'s', "Ф" },
+            {'t', "Ц" },
+            {'u', "Ъ" },
+            {'v', "Ы" },
+            {'w', "Ю" },
+            {'x', "Я" },
+            {'y', "Ә" },
+            {'z', "Ѫ" },
+            {'1', "А" },
+            {'2', "В" },
+            {'3', "Г" },
+            {'4', "Д" },
+            {'5', "Е" },
+            {'6', "Ѕ" },
+            {'7', "З" },
+            {'8', "И" },
+            {'9', "Ѳ" },
+        };
     }
 }
